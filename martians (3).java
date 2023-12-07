ba8af5e30d429dddc4995fa6f1660f7afe819482
Пользователь <nursultanadilbekov5@gmail.com>
2023-12-07 18:44:56 +0600

    seventh commit

--- martians (3).java
+++ martians (3).java
@@ -20,7 +20,6 @@ public class Main {
             System.out.println("Attemps: " + attempts + " \nEnter the kilometer marks to check cargo location: ");
             int[] numbers = guessNumbers();
 
-            //This loop for check your guesses with the real locations of cargo.
             for(int i = 0; i < cargoLocations.length; i++) {
                 for(int j = 0; j < numbers.length; j++){
                     if(cargoLocations[i] == numbers[j]){
@@ -38,3 +37,38 @@ public class Main {
                 System.out.println("You guessed " + rightGuess + " kilometers marks");
                 rightGuess = 0;
             }
+
+            if(attempts == 5) {
+                System.out.println("Your attempts exhausted. Boxes changing locations... ");
+                changeLocations(cargoLocations);
+                System.out.println("Cargo have changed location. Try again!");
+                attempts = 0;
+            }
+            cargoLocations = Arrays.copyOf(storedCargoLocations, cargoLocations.length);
+            attempts++;
+        }
+
+    }
+    public static int[] guessNumbers(){
+        Scanner scanner = new Scanner(System.in);
+        int guess1 = scanner.nextInt();
+        int guess2 = scanner.nextInt();
+        int guess3 = scanner.nextInt();
+        return new int[]{guess1, guess2, guess3};
+
+    }
+    public static void changeLocations(int[] cargoLocations){
+        Random random = new Random();
+        for(int i = 0; i < cargoLocations.length; i++) {
+           cargoLocations[i] = random.nextInt(6)  + 1;
+        }
+    }
+    public static int[] generateRandomLocations() {
+        Random random = new Random();
+        int[] locations = new int[3];
+        for (int i = 0; i < locations.length; i++) {
+            locations[i] = random.nextInt(6) + 1;
+        }
+        return locations;
+    }
+}
